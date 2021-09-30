9d5c6d4209d9d19f7263641da2191a3c83cb762b
Gabriel Santana <gabriel_santana17@hotmail.com>
2021-09-30 18:49:31 -0300

    Add files via upload

---
+++ função potenciação.js
@@ -0,0 +1,11 @@
+let num1;
+let num2;
+
+function potencia(num1,num2){
+    let resultado = Math.pow(num1,num2);
+    console.log(num1 + " elevado a " + num2 + " é " + resultado);
+}
+
+potencia(2,2);
+potencia(2,3);
+potencia(3,2);
