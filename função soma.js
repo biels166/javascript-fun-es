42080c9884ba1d1d451114fc4018b0a41d48b389
Gabriel Santana <gabriel_santana17@hotmail.com>
2021-09-30 16:29:14 -0300

    Add files via upload

---
+++ função soma.js
@@ -0,0 +1,17 @@
+let a;
+let b;
+
+
+function soma(a,b){
+    let resultado = a + b;
+    console.log("O resultado é " + resultado);
+}
+
+function somadois(a,b){
+    return a + b;
+}
+
+soma(1,1);
+soma(23,57);
+
+console.log("A soma é: " + somadois(1,4));
